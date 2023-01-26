f65146d6b64196705795b3861dd9b49ad5e87b8e
arunvdisprz <121215098+arunvdisprz@users.noreply.github.com>
2023-01-26 22:05:50 +0530

    Update AppointmentsBL.cs

--- DisprzTraining/Business/AppointmentsBL.cs
+++ DisprzTraining/Business/AppointmentsBL.cs
@@ -8,36 +8,38 @@ namespace DisprzTraining.Business
         {
             new AppointmentList()
             {
-                name = "string",
+                name = "Arun",
                 id = "a06ac7bd-1b6c-4443-a499-deccd3f35660",
                 appointmentDate = new DateTime(2023, 1, 12),
                 appointmentStartTime = new DateTime(2023, 1, 12, 23, 00, 00),
                 appointmentEndTime = new DateTime(2023, 1, 12, 23, 59, 00),
-                appointmentContent = "string",
-                location = "string",
-                description = "string",
-                color = "string",
+                appointmentContent = "Meet with mentor",
+                location = "Office",
+                description = "nil",
+                color = "#ff0055",
                 appointmentStatus = true
             },
             new AppointmentList()
             {
-                name = "string",
+                name = "Arun",
                 id = "a06ac7bd-1b6c-4443-a499-deccd3f35661",
                 appointmentDate = new DateTime(2023, 1, 12),
                 appointmentStartTime = new DateTime(2023, 1, 12, 13, 00, 00),
                 appointmentEndTime = new DateTime(2023, 1, 12, 13, 59, 00),
-                appointmentContent = "string",
-                location = "string",
-                description = "string",
-                color = "string",
+                appointmentContent = "QA Session",
+                location = "G meet",
+                description = "nil",
+                color = "#ff0055",
                 appointmentStatus = true
             }
         };
 
         public static List<AppointmentList> filteredAppointmentList = new List<AppointmentList> { };
 
-        //Retrieves all appointments in the list asynchronously.
-        //The task result contains a list of appointments sorted by appointment date.
+        /// <summary>
+        /// A method called "GetAllAppointmentInList" that returns a sorted list of all appointments in the collection "allAppointmentList" by start time.
+        /// </summary>
+        /// <returns>List of AppointmentList: returns a sorted list of all appointments in the collection "allAppointmentList" by start time.</returns>
         public List<AppointmentList> GetAllAppointmentInList()
         {
             allAppointmentList.Sort(
@@ -46,8 +48,11 @@ namespace DisprzTraining.Business
             return allAppointmentList;
         }
 
-        ///Retrieves appointments by date in the list asynchronously.
-        //The task result contains a list of appointments filtered by the specified date and sorted by appointment start time.
+        /// <summary>
+        /// A method called "GetAppointmentByDateInList" that takes a DateTime and returns a list of appointments from the collection "allAppointmentList" that have the same date as the input.
+        /// </summary>
+        /// <param name="date">The date to filter the appointments by.</param>
+        /// <returns>List of AppointmentList: returns a list of appointments that have the same date as the input and are ordered by start time.</returns>
         public List<AppointmentList> GetAppointmentByDateInList(DateTime date)
         {
             filteredAppointmentList = allAppointmentList
@@ -61,9 +66,11 @@ namespace DisprzTraining.Business
             return filteredAppointmentList;
         }
 
-        // Adds an appointment to the list.
-        // A Boolean value indicating whether the appointment was successfully added (True) or
-        //If the appointment conflicts with an existing appointment (False).
+        /// <summary>
+        /// A method called "AddAppointmentInList" that takes an object of type "AppointmentList" and adds an appointment to a collection "allAppointmentList" and also checks for time conflict.
+        /// </summary>
+        /// <param name="addAppointmentValue">An object that holds the values for the new appointment to be added to the collection.</param>
+        /// <returns>Boolean: returns 'true' if an appointment is successfully added and 'false' if the addition is not successful due to a time conflict.</returns>
         public Boolean AddAppointmentInList(AppointmentList addAppointmentValue)
         {
             filteredAppointmentList = allAppointmentList
@@ -93,12 +100,14 @@ namespace DisprzTraining.Business
                 return (false);
         }
 
-        //The CheckCondition method is a function that takes four DateTime parameters: newStartTime, newEndTime, StartTime, and EndTime.
-        //These parameters represent the start and end times of two appointments that the method will compare to determine if they are overlapping.
-        //The method first calculates the middle time of the new appointment by adding the difference between the end
-        //And start minutes of the new appointment, divided by 2, to the start time of the new appointment.
-        // If any of these conditions are true, the method will return true, indicating that the appointments are overlapping.
-        // If none of these conditions are true, the method will return false, indicating that the appointments are not overlapping.
+        /// <summary>
+        /// A method called "CheckCondition" that takes four arguments and checks if there is a time conflict between the new start and end times and the existing start and end times.
+        /// </summary>
+        /// <param name="newStartTime">The new start time for the appointment.</param>
+        /// <param name="newEndTime">The new end time for the appointment.</param>
+        /// <param name="startTime">The existing start time for the appointment.</param>
+        /// <param name="endTime">The existing end time for the appointment.</param>
+        /// <returns>Boolean: returns 'true' if there is a time conflict and 'false' if there is no conflict.</returns>
         public Boolean CheckCondition(
             DateTime newStartTime,
             DateTime newEndTime,
@@ -109,9 +118,11 @@ namespace DisprzTraining.Business
             return (newStartTime < endTime && newEndTime > startTime);
         }
 
-        // Updates an appointment in the list.
-        //A Boolean value indicating whether the appointment was successfully updated (True) or
-        //If the updated appointment conflicts with an existing appointment (False).
+        /// <summary>
+        /// A method called "patchAppointmentsInList" that takes an object of type "PatchAppointmentList" and updates an appointment from a collection "allAppointmentList" based on its matching id and also check for the time conflict.
+        /// </summary>
+        /// <param name="patchAppointmentValue">An object that holds the new values for the appointment and the id of the appointment to be updated in the collection.</param>
+        /// <returns>Boolean: returns 'true' if an appointment is successfully updated and 'false' if the update is not successful.</returns>
         public Boolean patchAppointmentsInList(PatchAppointmentList patchAppointmentValue)
         {
             filteredAppointmentList = allAppointmentList
@@ -137,6 +148,7 @@ namespace DisprzTraining.Business
 
             if (!checkList && appointment != null)
             {
+                appointment.appointmentDate = patchAppointmentValue.patchAppointmentDate;
                 appointment.appointmentStartTime = patchAppointmentValue.patchAppointmentStartTime;
                 appointment.appointmentEndTime = patchAppointmentValue.patchAppointmentEndTime;
                 appointment.appointmentContent = patchAppointmentValue.patchAppointmentContent;
@@ -150,12 +162,17 @@ namespace DisprzTraining.Business
             }
         }
 
-        //deleteId : A string value representing the ID of the appointment that needs to be deleted.deleteId .
-        //Boolean - Returns true if the appointment was deleted successfully, false otherwise.
-        public Boolean DeleteAppointmentById(string deleteId)
+        /// <summary>
+        /// A method called "DeleteAppointmentById" that takes a string argument "appointmentId" and deletes an appointment from a collection "allAppointmentList" based on its matching id.
+        /// </summary>
+        /// <param name="appointmentId">The id of the appointment to be deleted from the collection.</param>
+        /// <returns>Boolean: returns 'true' if an appointment is successfully deleted and 'false' if no matching appointment is found in the collection.</returns>
+        public Boolean DeleteAppointmentById(string appointmentId)
         {
             //It returns the index of the first element in the collection that matches the specified condition, or -1 if no element is found.
-            var index = allAppointmentList.FindIndex(appointment => appointment.id == deleteId);
+            var index = allAppointmentList.FindIndex(
+                appointment => appointment.id == appointmentId
+            );
 
             if (index == -1)
                 return false;
